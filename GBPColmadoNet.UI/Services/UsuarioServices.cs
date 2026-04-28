a97da2ed0b54e98e70feb83e65df98cf8e3a4d21
Juniorbp20 <Jbonifacio0707@gmail.com>
2026-04-28 18:55:07 -0400

    Agregar UsuarioServices con CRUD; hacer que CategoriaService sea asíncrono
    Se agregó la clase UsuarioServices con operaciones CRUD completas para la entidad Usuario usando EF Core. Se actualizó el método Buscar de CategoriaService para que sea asíncrono, mejorando así el rendimiento y la consistencia.

---
+++ GBPColmadoNet.UI/Services/UsuarioServices.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Aplicada1.Core;
+using GBPColmadoNet.Data.Context;
+using GBPColmadoNet.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GBPColmadoNet.UI.Services
+{
+    public class UsuarioServices(ColmadoContext context
+    ) : IService<Usuario, int>
+    {
+        public async Task<bool> Guardar(Usuario entidad)
+        {
+            if (!await Existe(entidad.UsuarioId))
+                return await Insertar(entidad);
+            else
+                return await Modificar(entidad);
+        }
+
+        private async Task<bool> Insertar(Usuario entidad)
+        {
+            context.Usuarios.Add(entidad);
+            return await context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> Existe(int id)
+        {
+            return await context.Usuarios.AnyAsync(a => a.UsuarioId == id);
+        }
+
+        public async Task<bool> Modificar(Data.Models.Usuario entiad)
+        {
+            context.Usuarios.Update(entiad);
+            return await context.SaveChangesAsync() > 0;
+        }
+        
+        public async Task<Usuario?> Buscar(int id)
+        {
+            return await context.Usuarios
+                .AsNoTracking()
+                .FirstOrDefaultAsync(c => c.UsuarioId == id);
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            var usuario = await context
+                .Usuarios
+                .FindAsync(id);
+
+            if (usuario == null)
+                return false;
+
+            context.Usuarios.Remove(usuario);
+            var cantidad = await context.SaveChangesAsync();
+
+            return cantidad > 0;
+        }
+
+        public async Task<List<Usuario>> GetList(Expression<Func<Usuario, bool>> criterio)
+        {
+            return await context.Usuarios
+                .AsNoTracking()
+                .Where(criterio)
+                .ToListAsync();
+        }
+    }
+}
